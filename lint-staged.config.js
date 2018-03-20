fffe062a660a7672d38f738cf95394bd5174abcb
Eric Ren <546296+rencire@users.noreply.github.com>
2018-03-19 23:24:56 -0700

    Rename config files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,9 @@
-{
+module.exports = {
   "*.{ts,tsx}": [
     "prettier --write",
     "tslint -c tslint.json 'src/**/*.ts' --fix",
     "git add",
-    "jest -c .jestrc.json --bail --findRelatedTests"
+    "jest --bail --findRelatedTests"
   ],
   "*.{json,md}": ["prettier --write", "git add"]
-}
+};
